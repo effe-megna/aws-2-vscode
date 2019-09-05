1dae2f057423263d8a374899f16b58f5bd988e06
Francesco Megna <francesco.megna@mavigex.com>
2019-09-05 18:02:53 +0200

    initial treeDataProvider

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,8 @@
 import * as vscode from 'vscode';
 import * as TE from "fp-ts/lib/TaskEither";
 import * as E from "fp-ts/lib/Either";
-import * as T from "fp-ts/lib/Task";
-import * as P from "fp-ts/lib/pipeable";
-import { exec } from "child_process";
-import { toError } from 'fp-ts/lib/Either';
-import { safeExec, thenableToPromise, withProgress } from './utils';
+import { pipe } from "fp-ts/lib/pipeable";
+import { safeExec, parseJsonTE, monadvsCode } from './utils';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -14,61 +11,66 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.CloudWatchLogs', async () => {
 		console.log('Start');
 
-		const res = await P.pipe(
-			safeExec("aws logs describe-log-groups --query logGroups[*].logGroupName"),
-			TE.chain(v =>
-				TE.fromEither(
-					E.parseJSON(v, toError) as E.Either<Error, string[]>
+		const a = new DepNodeProvider("stream-logs");
+
+		vscode.window.registerTreeDataProvider("streamLogs", a);
+
+		// const a = vscode.window.createTreeView('1', {
+		// 	canSelectMany: true,
+		// 	showCollapseAll: true,
+		// 	treeDataProvider: {
+		// 		getChildren: () => ["1", "2", "3"],
+		// 		getParent: () => null,
+		// 		getTreeItem: (el) => ({ label: "LABEL" })
+		// 	}
+		// });
+
+		// vscode.window.registerTreeDataProvider("1", {
+		// 	getChildren: () => ["1", "2", "3"],
+		// 	getParent: () => null,
+		// 	getTreeItem: (el) => ({ label: "LABEL" })
+		// });
+
+		const res = await pipe(
+			pipe(
+				safeExec("aws logs describe-log-groups --query logGroups[*].logGroupName"),
+				monadvsCode.window.withProgress()
+			),
+			TE.chain(v => parseJsonTE<string[]>(v)),
+			TE.chain(monadvsCode.window.showQuickPick),
+			TE.chain(logGroup =>
+				pipe(
+					safeExec(`aws logs describe-log-streams --log-group-name ${logGroup} --query logStreams[*].logStreamName`),
+					TE.map(v => ({ logGroup, logStreams: v })),
+					monadvsCode.window.withProgress()
 				)
 			),
-			TE.chain(logGroups => TE.tryCatch(
-				async () => {
-					const selection = await thenableToPromise(vscode.window.showQuickPick(logGroups));
-
-					if (selection === undefined || selection === null) {
-						throw new Error("missing selection");
-					} else {
-						return selection;
-					}
-				},
-				toError
-			)),
-			TE.chain(logGroup =>
-				P.pipe(
-					safeExec(`aws logs describe-log-streams --log-group-name ${logGroup} --query logStreams[*].logStreamName`),
+			TE.chain(({ logGroup, logStreams }) =>
+				pipe(
+					parseJsonTE<string[]>(logStreams),
 					TE.map(v => ({ logGroup, logStreams: v }))
 				)
 			),
 			TE.chain(({ logGroup, logStreams }) =>
-				P.pipe(
-					TE.fromEither(
-						E.parseJSON(logStreams, toError) as E.Either<Error, string[]>
-					),
-					TE.map(v => ({ logGroup, logStreams: v }))
+				pipe(
+					monadvsCode.window.showQuickPick(logStreams),
+					TE.map(v => ({ logGroup, streamSelected: v }))
 				)
 			),
-			TE.chain((userSelection) => TE.tryCatch(
-				async () => {
-					const selection = await thenableToPromise(vscode.window.showQuickPick(userSelection.logStreams));
-
-					if (selection === undefined || selection === null) {
-						throw new Error("missing selection");
-					} else {
-						return { ...userSelection, streamSelected: selection };
-					}
-				},
-				toError
-			)),
 			TE.chain(({ logGroup, streamSelected }) =>
-				safeExec(`aws logs get-log-events --log-group-name '${logGroup}' --log-stream-name '${streamSelected}'`)
+				pipe(
+					safeExec(`aws logs get-log-events --log-group-name '${logGroup}' --log-stream-name '${streamSelected}'`),
+					monadvsCode.window.withProgress(),
+				)
 			)
 		)();
 
-		P.pipe(
+		pipe(
 			res,
 			E.fold(
 				(e) => vscode.window.showErrorMessage(e.message),
 				(v) => {
+					console.log(v);
 					return vscode.window.showInformationMessage(v);
 				}
 			),
@@ -78,6 +80,56 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+export class DepNodeProvider implements vscode.TreeDataProvider<LogStreamItem> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<LogStreamItem | undefined> = new vscode.EventEmitter<LogStreamItem | undefined>();
+	readonly onDidChangeTreeData: vscode.Event<LogStreamItem | undefined> = this._onDidChangeTreeData.event;
+
+	constructor(private workspaceRoot: string) { }
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: LogStreamItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: LogStreamItem): Thenable<LogStreamItem[]> {
+		return Promise.resolve([
+			this.toDep("1"),
+			this.toDep("2"),
+			this.toDep("3"),
+			this.toDep("4"),
+			this.toDep("5"),
+		]);
+	}
+
+	toDep = (label: string): LogStreamItem => {
+		return new LogStreamItem(label, vscode.TreeItemCollapsibleState.Collapsed);
+	}
+}
+
+export class LogStreamItem extends vscode.TreeItem {
+	constructor(
+		public readonly label: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+	}
+
+	get tooltip(): string {
+		return `${this.label}`;
+	}
+
+	// iconPath = {
+	// 	light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+	// 	dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	// };
+
+	contextValue = 'dependency';
+}
 
 
 // this method is called when your extension is deactivated
--- src/utils.ts
+++ src/utils.ts
@@ -1,81 +1,127 @@
-import { window, ProgressLocation } from "vscode";
+import { pipe } from "fp-ts/lib/pipeable";
 import * as TE from "fp-ts/lib/TaskEither";
+import * as E from 'fp-ts/lib/Either';
+import { window, ProgressLocation } from "vscode";
 import { exec } from "child_process";
-import { toError, Either } from 'fp-ts/lib/Either';
 
-type withProgress = <A>(params: {
-  taskEither: TE.TaskEither<Error, A>,
-  title?: string,
-  onCancel?: () => unknown
-}) => TE.TaskEither<Error, A>;
+// export const withProgressTE = <A>({
+//   title = "🎅 Santa Claus is coming to town",
+//   onCancel = undefined
+// }: { title?: string, onCancel?: () => TE.TaskEither<Error, A> }) => (taskEither: TE.TaskEither<Error, A>): TE.TaskEither<Error, A> =>
+//     //@ts-ignore
+//     TE.tryCatch(
+//       () => {
+//         const action = new Promise((resolve, reject) => {
+//           window.withProgress({
+//             location: ProgressLocation.Notification,
+//             title,
+//             cancellable: onCancel !== undefined
+//           }, (_, token) => {
+//             if (onCancel) {
+//               token.onCancellationRequested(onCancel);
+//             }
+//             const res = taskEither();
 
-//@ts-ignore
-export const withProgress: withProgress = ({
-  taskEither,
-  title = "Santa Claus is coming to town",
-  onCancel
-}) => {
+//             res
+//               .then(v => {
+//                 pipe(
+//                   v,
+//                   E.fold(
+//                     (e) => { throw e; },
+//                     (a) => resolve(a)
+//                   )
+//                 );
+//               });
 
-  return TE.tryCatch(
+//             return res;
+//           });
+//         });
+
+//         return action;
+//       },
+//       E.toError
+//     );
+
+export const withProgressTE = <A>() => (taskEither: TE.TaskEither<Error, A>): TE.TaskEither<Error, A> =>
+  //@ts-ignore
+  TE.tryCatch(
     () => {
-      const action = new Promise<Either<Error, any>>((res, rej) => {
+      const action = new Promise((resolve, reject) => {
         window.withProgress({
           location: ProgressLocation.Notification,
-          title,
-          cancellable: onCancel !== undefined
+          title: "🎅 Santa Claus is coming to town",
         }, (_, token) => {
-          if (onCancel) {
-            token.onCancellationRequested(onCancel);
-          }
           const res = taskEither();
 
-          //@ts-ignore
-          res.then(v => res(v));
-      
+          res
+            .then(v => {
+              pipe(
+                v,
+                E.fold(
+                  (e) => { throw e; },
+                  (a) => resolve(a)
+                )
+              );
+            });
+
           return res;
         });
       });
 
       return action;
     },
-    toError
+    E.toError
   );
 
-
-  const action = window.withProgress({
-    location: ProgressLocation.Notification,
-    title,
-    cancellable: onCancel !== undefined
-  }, (_, token) => {
-    if (onCancel) {
-      token.onCancellationRequested(onCancel);
-    }
-
-    return taskEither();
-  });
-
-  return TE.tryCatch(
-    () => thenableToPromise(action),
-    toError
-  );
-};
 
 export const thenableToPromise = <T>(thenable: Thenable<T>) => new Promise<T>((res, rej) => {
-	thenable.then((v) => res(v), (reasons) => rej(reasons));
+  thenable.then((v) => res(v), (reasons) => rej(reasons));
 });
 
 export const safeExec = (cmd: string): TE.TaskEither<Error, string> =>
-	TE.tryCatch(
-		() => new Promise<string>((res, rej) => {
-			exec(cmd, (err, stdout, stderr) => {
-				if (err) {
-					rej(err);
-				} else {
-					res(stdout);
-				}
+  TE.tryCatch(
+    () => new Promise<string>((res, rej) => {
+      exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+          rej(err);
+        } else {
+          res(stdout);
+        }
 
-				console.log("STDERR ", stderr);
-			});
-		}),
-		toError
-	);
+        console.log("STDERR ", stderr);
+      });
+    }),
+    E.toError
+  );
+
+export const parseJsonTE = <A>(v: string): TE.TaskEither<Error, A> =>
+  TE.fromEither(
+    E.parseJSON(v, E.toError) as E.Either<Error, A>
+  );
+
+export interface MonadVsCode {
+  window: {
+    showQuickPick: (items: string[]) => TE.TaskEither<Error, string>,
+    withProgress: <A>() => (t: TE.TaskEither<Error, A>) => TE.TaskEither<Error, A>
+  };
+}
+
+const showQuickPickTE = (items: string[]) => TE.tryCatch(
+  async () => {
+    const selection = await thenableToPromise(window.showQuickPick(items));
+
+    if (selection === undefined || selection === null) {
+      throw new Error("missing selection");
+    } else {
+      return selection;
+    }
+  },
+  E.toError
+);
+
+export const monadvsCode: MonadVsCode = {
+  window: {
+    showQuickPick: showQuickPickTE,
+    withProgress: withProgressTE
+  }
+};
